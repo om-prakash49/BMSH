3f45c88583ed89f2d34c2ae1dbd551a370663408
om-prakash49 <omprakash.jat@swiftrut.com>
2025-01-02 02:36:28 +0530

    completed our promise and service section

---
+++ assets/js/service.js
@@ -0,0 +1,31 @@
+// Carousel Logic
+$(document).ready(function () {
+    $('.multiple-items').slick({
+      infinite: true, 
+      slidesToShow: 4, 
+      slidesToScroll: 1, 
+      dots: false, 
+      arrows: true,
+      prevArrow:
+        '<button class="slick-prev bg-gray-300 rounded-full w-10 h-10 flex items-center justify-center text-black shadow-md hover:bg-gray-200"><</button>',
+      nextArrow:
+        '<button class="slick-next bg-gray-300 rounded-full w-10 h-10 flex items-center justify-center text-black shadow-md hover:bg-gray-200">></button>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2, // Show 2 slides for medium devices
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1, // Show 1 slide for small devices
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+  });
+  
